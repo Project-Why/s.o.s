9a41f4a16ef4d92909d6b7122d7f27df3f1da942
kadiace <rlacjfghks95@gmail.com>
2024-04-02 21:54:46 +0900

    Set all morse code image.

--- src/components/Cockpit/Display/MorseInfo.tsx
+++ src/components/Cockpit/Display/MorseInfo.tsx
@@ -1,5 +1,13 @@
 import MorseDot1 from 'assets/images/Cockpit/Display/Decrypting/Morse_Dot1.gif';
+import MorseDot2 from 'assets/images/Cockpit/Display/Decrypting/Morse_Dot2.gif';
+import MorseDot3 from 'assets/images/Cockpit/Display/Decrypting/Morse_Dot3.gif';
+import MorseDot4 from 'assets/images/Cockpit/Display/Decrypting/Morse_Dot4.gif';
+import MorseDot5 from 'assets/images/Cockpit/Display/Decrypting/Morse_Dot5.gif';
 import MorseLine1 from 'assets/images/Cockpit/Display/Decrypting/Morse_Line1.gif';
+import MorseLine2 from 'assets/images/Cockpit/Display/Decrypting/Morse_Line2.gif';
+import MorseLine3 from 'assets/images/Cockpit/Display/Decrypting/Morse_Line3.gif';
+import MorseLine4 from 'assets/images/Cockpit/Display/Decrypting/Morse_Line4.gif';
+import MorseLine5 from 'assets/images/Cockpit/Display/Decrypting/Morse_Line5.gif';
 
 import { CSSProperties } from 'react';
 
@@ -10,6 +18,8 @@ export type MorseInfoProps = {
 
 function MorseInfo(props: CSSProperties & MorseInfoProps) {
   const { index, code, ...cssProps } = props;
+  const dots = [MorseDot1, MorseDot2, MorseDot3, MorseDot4, MorseDot5];
+  const lines = [MorseLine1, MorseLine2, MorseLine3, MorseLine4, MorseLine5];
   return (
     <div id={`Signal ${index}`} style={{ ...cssProps }}>
       {code.split('\n').map((value, lineIndex) => (
@@ -27,18 +37,24 @@ function MorseInfo(props: CSSProperties & MorseInfoProps) {
             <div
               id={`Code ${codeIndex}`}
               key={`Code ${codeIndex}`}
-              style={{ width: '10%', aspectRatio: '1' }}
+              style={{
+                width: '10%',
+                aspectRatio: '1',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
             >
               {char === '1' && (
                 <img
-                  src={MorseLine1}
+                  src={lines[Math.floor(Math.random() * 5)]}
                   alt={`Code ${codeIndex}`}
                   style={{ width: '100%', objectFit: 'cover' }}
                 />
               )}
               {char === '0' && (
                 <img
-                  src={MorseDot1}
+                  src={dots[Math.floor(Math.random() * 5)]}
                   alt={`Code ${codeIndex}`}
                   style={{ width: '100%', objectFit: 'cover' }}
                 />
